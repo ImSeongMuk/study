a8a2f5312d4839f20d3521c90daf4103e168f047
ImSeongMuk <0326gg@gmail.com>
2019-10-08 11:21:27 +0900

    20191008-01

---
+++ 프로젝트/Bluetooth.kt
@@ -0,0 +1,28 @@
+package com.cookandroid.kotlinapp
+
+import android.Manifest
+import android.app.ListActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.bluetooth.*
+import android.bluetooth.BluetoothDevice
+import android.os.Handler
+
+
+class Bluetooth : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+
+        }
+
+
+}
